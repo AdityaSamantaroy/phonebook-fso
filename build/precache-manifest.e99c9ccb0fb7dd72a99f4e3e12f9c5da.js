9f24e07966335456a0c8f923d57e8a1d33e3f5d8
Aditya Samantaroy <adityasamantaroy@gmail.com>
2020-10-16 00:56:22 +0530

    add frontend

---
+++ build/precache-manifest.e99c9ccb0fb7dd72a99f4e3e12f9c5da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14751705c883dd5bbd6dd53fbd74a788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c592ef0f5f4673f5f1f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9af68b89fbfe292cb182",
+    "url": "/static/js/2.c7d8749e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7d8749e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c592ef0f5f4673f5f1f",
+    "url": "/static/js/main.af5b7a1e.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
